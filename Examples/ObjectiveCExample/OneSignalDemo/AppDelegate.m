c064cb9fd511d503b486d5de1630a0ea8705a613
Darren Tsung <darren@onesignal.com>
2018-06-06 11:12:40 -0700

    Send influx db event to test server

--- Examples/ObjectiveCExample/OneSignalDemo/AppDelegate.m
+++ Examples/ObjectiveCExample/OneSignalDemo/AppDelegate.m
@@ -41,6 +41,9 @@
     // (Optional) - Create block the will fire when a notification is recieved while the app is in focus.
     id notificationRecievedBlock = ^(OSNotification *notification) {
         NSLog(@"Received Notification - %@", notification.payload.notificationID);
+        
+        // HACKATHON: SEND TO INFLUX DB EVENT
+        [self sendToInfluxDBWithEvent:@"notification_received" WithNotificationId:notification.payload.notificationID];
     };
     
     // (Optional) - Create block that will fire when a notification is tapped on.
@@ -96,6 +99,27 @@
             handleNotificationAction:notificationOpenedBlock
                             settings:oneSignalSetting];
     return YES;
+}
+
+- (void)sendToInfluxDBWithEvent:(NSString*)event WithNotificationId:(NSString*)notificationID {
+    NSString *payload = [NSString stringWithFormat:@"events,player_id=%@ event_type=\"%@\",notification_id=\"%@\"", @"be9e6837-a2e4-493a-9321-7230e6341b90", event, notificationID];
+    NSData *postData = [payload dataUsingEncoding:NSUTF8StringEncoding];
+    NSString *postLength = [NSString stringWithFormat:@"%lu", [postData length]];
+    
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setHTTPMethod:@"POST"];
+    [request setURL:[NSURL URLWithString:@"http://147.75.192.45:8086/write?db=onesignal_dev"]];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    [request setHTTPBody:postData];
+    
+    NSLog(@"SENDING OFF INFLUXDB EVENT!");
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+    NSURLSessionDataTask *sendTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSLog(@"Received data: %@, response: %@, error: %@", data, response, error);
+    }];
+    [sendTask resume];
 }
 
 @end
